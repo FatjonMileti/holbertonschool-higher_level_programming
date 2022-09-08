5fce7104a1204cc29c87ae47b93b566e8362925a
FatjonMileti <tonimileti@yahoo.com>
2022-09-08 01:41:57 -0700

    ...

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    import sys
+    i = 1
+    if (len(sys.argv) > 1):
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("0 arguments.")
+        while i != len(sys.argv):
+            print("{}: {}".format(i, str(sys.argv[i])))
+            i += 1
