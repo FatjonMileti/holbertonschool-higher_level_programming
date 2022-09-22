3cdf938f8fe5f36ac22762e6153976837f3bca43
FatjonMileti <tonimileti@yahoo.com>
2022-09-22 04:31:36 -0700

    ...

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -62,7 +62,8 @@ class Rectangle:
         if self.__height == 0:
             return ""
 
-        rec = "\n".join([str(self.print_symbol) * self.__width for rows in range(self.__height)])
+        rec = "\n".join([str(self.print_symbol) * self.__width
+            for rows in range(self.__height)])
         return rec
 
     def __repr__(self):
