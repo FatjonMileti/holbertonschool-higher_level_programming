27602dfb50ec542b7a05bc43860d2c06fc016e7c
FatjonMileti <tonimileti@yahoo.com>
2022-09-28 01:33:34 -0700

    ...

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' *** *** '''
+BaseGeometry = __import__('9-rectangle').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' *** *** '''
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
