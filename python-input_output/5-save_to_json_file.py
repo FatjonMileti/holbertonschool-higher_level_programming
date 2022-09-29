444ecb03fac2e0602a3293017ff2dc47bbdd6c18
FatjonMileti <tonimileti@yahoo.com>
2022-09-29 03:25:31 -0700

    ...

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,6 +4,6 @@
 import json
 
 def save_to_json_file(my_obj, filename):
-	''' *** *** '''
-	with open(filename, "w") as f:
-		json.dump(my_obj, f)
+    ''' *** *** '''
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
