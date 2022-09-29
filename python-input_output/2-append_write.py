1cf653893e663671ce0501a30cba8f6cf347f1e7
FatjonMileti <tonimileti@yahoo.com>
2022-09-29 02:25:16 -0700

    ...

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-''' *** *** '''
+''' appends a string at the end of a text file '''
 
 
 def append_file(filename="", text=""):
-    ''' *** *** '''
+    ''' appends a string at the end of a text file '''
     with open(filename, 'a', encoding='utf-8') as f:
         return(f.write(text))
