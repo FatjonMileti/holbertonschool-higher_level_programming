6999bd63f22cd80c8fc16e1e43733a4bdfdcb69f
FatjonMileti <tonimileti@yahoo.com>
2022-09-29 05:42:42 -0700

    ...

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,17 +10,17 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        ''' *** *** '''
-	class_d = self.__dict__
-	sel_d = dict()
+        class_d = self.__dict__
+        sel_d = dict()
 
-	if type(attrs) is list:
-	    for attr in attrs:
-		if type(attr) is not str:
-		    return class_d
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
 
-		if attr in class_d:
-		    sel_d[attr] = class_d[attr]
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
 
-	    return sel_d
-	return class_d
+            return sel_d
+
+        return class_d
