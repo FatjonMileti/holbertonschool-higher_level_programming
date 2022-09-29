d53c97179c2598fb77261d35170de72e48744f73
FatjonMileti <tonimileti@yahoo.com>
2022-09-29 04:11:27 -0700

    ...

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' *** *** '''
+from os import path
+from sys import argv
+
+
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+if path.exist('add_item.json'):
+    jfile = load_from_json_file('add_item.json')
+
+else:
+    jfile = []
+
+for i in range(1, len(argv)):
+    jfile.append(argv[i])
+
+save_to_json_file('jfile')
