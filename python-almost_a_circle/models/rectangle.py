c9fd26b59c5f5131a6f12ddded36d3c69339402a
FatjonMileti <tonimileti@yahoo.com>
2022-10-03 03:40:30 -0700

    ...

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,10 @@ class Rectangle(Base):
     def area(self):
         ''' *** *** '''
         return self.__width * self.__height
+    def display(self):
+        ''' *** *** '''
+        for i in range(self.__y):
+            print()
+        for j in range(self.__height):
+            print('' * self.__x, end ='')
+            print('#' * self.__width)
