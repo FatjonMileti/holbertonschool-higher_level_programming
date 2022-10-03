a37a0777edb96a660fea27d18a69a8da06a4e917
FatjonMileti <tonimileti@yahoo.com>
2022-10-03 01:58:02 -0700

    ...

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ class Base:
     ''' *** *** '''
     _nb_objects = 0
     def __init__(self, id=None):
+
         if id is None:
             Base._nb_objects += 1
             self.id = Base._nb_objects
