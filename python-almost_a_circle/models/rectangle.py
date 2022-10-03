802efcfe8001675ab3f34b77babb3750e67633e0
FatjonMileti <tonimileti@yahoo.com>
2022-10-03 02:44:42 -0700

    ...

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,14 +19,24 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if width is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError('width must be > 0')
         self.__width = width
 
     @property
     def height(self):
+
         return self.__height
 
     @height.setter
     def height(self, height):
+        if height is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError('height must be > 0')
+
         self.__height = height
 
     @property
@@ -35,6 +45,11 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if x is not int:
+            raise TypeError("x must be an integer")
+        if x <= 0:
+            raise ValueError('x must be > 0')
+
         self.__x = x
 
     @property
@@ -43,4 +58,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if y is not int:
+            raise TypeError("y must be an integer")
+        if y <= 0:
+            raise ValueError('y must be > 0')
+
         self.__y = y
