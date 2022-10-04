e2d272a3f6392e0f5757577dac40ee713b8a1df8
FatjonMileti <tonimileti@yahoo.com>
2022-10-04 01:53:16 -0700

    ...

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''' *** *** '''
+import json
 
 
 class Base:
@@ -12,3 +13,11 @@ class Base:
             self.id = Base._nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' *** *** '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
