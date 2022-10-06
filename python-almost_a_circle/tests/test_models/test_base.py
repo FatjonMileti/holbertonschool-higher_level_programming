09a61c2837f5a2a133ec9ecc5b63e064a9c4e7a5
FatjonMileti <tonimileti@yahoo.com>
2022-10-06 01:32:28 -0700

    ...

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,12 +2,16 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def test_1(self):
+    def test_1_1(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
-    def test_2(self):
+    def test_1_2(self):
         b2 = Base()
         self.assertEqual(b2.id, 2)
-    def test_3(self):
+    def test_1_3(self):
         b3 = Base(89)
         self.assertEqual(b3.id, 89)
+    def test_2_1(self):
+        self.assertEqual(Base.to_json_string(None), "null")
+    def test_2_2(self):
+        self.assertEqual(Base.to_json_string([]),'[]')
