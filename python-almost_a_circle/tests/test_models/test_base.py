3749dc2909c9b597729c19368e3e21e4284a430d
FatjonMileti <tonimileti@yahoo.com>
2022-10-06 00:56:33 -0700

    ...

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,34 +2,23 @@
 """Test module"""
 import io, sys, unittest, os
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    def test_no_args(self):
+    def test_1(self):
         base1 = Base()
         base2 = Base()
         self.assertEqual(base1.id, base2.id - 1)
 
-    def test_none_id(self):
+    def test_2(self):
         base1 = Base(None)
         base2 = Base(None)
         self.assertEqual(base1.id, base2.id - 1)
-
-    def test_three_base(self):
-        base1 = Base()
-        base2 = Base()
-        base3 = Base()
-        self.assertEqual(base1.id, base3.id - 2)
-
-    def test_id_uniq(self):
-        self.assertEqual(20, Base(20).id)
     
-    def test_id_pub(self):
-        base1 = Base(15)
-        base1.id = 20
-        self.assertEqual(20, base1.id)
+    def test_3(self):
+        base1 = Base(12)
+        base1.id = 10
+        self.assertEqual(10, base1.id)
 
     def test_to_json_string_none(self):
         self.assertEqual("[]", Base.to_json_string(None))
