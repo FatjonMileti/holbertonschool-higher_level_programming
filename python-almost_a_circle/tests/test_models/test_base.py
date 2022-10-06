51cbe8f6bd3b563ea376b4024ed3577e4f45dafa
FatjonMileti <tonimileti@yahoo.com>
2022-10-06 00:45:55 -0700

    ...

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,12 @@
-import imp
 import unittest
 import os
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def test_base_assignments(self):
+    def test_1(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
         b2 = Base()
         self.assertEqual(b2.id, 2)
-        b3 = Base(89)
-        self.assertEqual(b3.id, 89)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
