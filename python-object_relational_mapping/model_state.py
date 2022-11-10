235cc372920469001c1fd1370acb3fb3b6f5f9ae
FatjonMileti <tonimileti@yahoo.com>
2022-11-10 03:58:52 -0800

    ...

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    """class State that maps to table states"""
+    ''' *** *** '''
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True)
