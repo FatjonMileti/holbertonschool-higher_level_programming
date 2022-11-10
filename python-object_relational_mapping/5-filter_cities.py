645c154b6d3198ebdcc021b3c88a54c854e4ee79
FatjonMileti <tonimileti@yahoo.com>
2022-11-10 01:22:47 -0800

    ...

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' *** *** '''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("""
+            SELECT cities.name FROM cities, states
+            WHERE states.id = cities.state_id AND states.name = %s
+            ORDER BY cities.id
+        """, (argv[4], ))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
