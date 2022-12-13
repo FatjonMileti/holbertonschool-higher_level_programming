1fc5ecd7a6f2a77c33800bc27978b50278ed5d42
FatjonMileti <tonimileti@yahoo.com>
2022-12-13 01:20:29 -0800

    ...

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
+
+request(url, (err, res, body) => {
+    if (err) throw err;
+
+    console.log(JSON.parse(body).title);
+});
