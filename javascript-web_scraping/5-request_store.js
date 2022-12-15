53ee0c388708a2ac0a624666f7c8f32fa6aacb9a
FatjonMileti <tonimileti@yahoo.com>
2022-12-15 01:24:07 -0800

    ...

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,8 +3,6 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-
 request(process.argv[2], (err, res, body) => {
   if (err) throw err;
   fs.writeFile(process.argv[3], body, 'utf-8', (err, data) => {
